a939eef07895af52b17d84fe3f277580c32009cb
Emily Hontoria <hontoria@ccs.neu.edu>
2018-10-17 22:26:51 -0400

    Basic generation and bootstrap

---
+++ priv/repo/migrations/20181018015135_create_tasks.exs
@@ -0,0 +1,13 @@
+defmodule TaskTracker.Repo.Migrations.CreateTasks do
+  use Ecto.Migration
+
+  def change do
+    create table(:tasks) do
+      add :name, :string
+      add :done, :boolean, default: false, null: false
+
+      timestamps()
+    end
+
+  end
+end
