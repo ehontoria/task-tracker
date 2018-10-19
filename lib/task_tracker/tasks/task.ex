bc440ac94256b182fb2a12eefbdef6adb59d044e
hontoriae <hontoria.e@husky.neu.edu>
2018-10-19 00:45:37 -0400

    Adding user to tasks

--- lib/task_tracker/tasks/task.ex
+++ lib/task_tracker/tasks/task.ex
@@ -7,13 +7,15 @@ defmodule TaskTracker.Tasks.Task do
     field :done, :boolean, default: false
     field :name, :string
 
+    belongs_to :user, TaskTracker.Users.User
+
     timestamps()
   end
 
   @doc false
   def changeset(task, attrs) do
     task
-    |> cast(attrs, [:name, :done])
-    |> validate_required([:name, :done])
+    |> cast(attrs, [:name, :done, :user_id])
+    |> validate_required([:name, :done, :user_id])
   end
 end
--- priv/repo/migrations/02_create_tasks.exs
+++ priv/repo/migrations/02_create_tasks.exs
@@ -5,6 +5,7 @@ defmodule TaskTracker.Repo.Migrations.CreateTasks do
     create table(:tasks) do
       add :name, :string
       add :done, :boolean, default: false, null: false
+      add :user_id, references(:users, on_delete: :delete_all), null: true
 
       timestamps()
     end
